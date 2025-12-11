c52ca73c6d8475ba7527a1025219a6cdf27f0ea4
Jose HG Rodrigues <josehgrodrigues3@gmail.com>
2025-12-10 23:17:15 -0300

    modulo para processar o .geo

--- src/geo.c
+++ src/geo.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "figure.h"
-#include "queue.h"
+#include "list.h"
 
 #define MAX_LINE_BUFFER 512
+
 typedef struct {
   char family[64];
   char weight[3];
@@ -15,9 +15,11 @@ typedef struct {
 
 static Ts *tsInit() {
   Ts *t = (Ts *)malloc(sizeof(Ts));
-  strcpy(t->family, "Arial");
-  strcpy(t->weight, "n");
-  t->size = 12;
+  if (t) {
+      strcpy(t->family, "Arial");
+      strcpy(t->weight, "n");
+      t->size = 12;
+  }
   return t;
 }
 
@@ -25,8 +27,7 @@ static Figure processCircle(const char *line) {
   int id;
   double x, y, r;
   char colorB[32], colorF[32];
-  if (sscanf(line, "c %d %lf %lf %lf %s %s", &id, &x, &y, &r, colorB, colorF) ==
-      6) {
+  if (sscanf(line, "c %d %lf %lf %lf %s %s", &id, &x, &y, &r, colorB, colorF) == 6) {
     Figure newFig = figureInit(CIRCLE);
     setCircle(newFig, id, x, y, r, colorB, colorF);
     return newFig;
@@ -38,8 +39,7 @@ static Figure processRectangle(const char *line) {
   int id;
   double x, y, w, h;
   char colorB[32], colorF[32];
-  if (sscanf(line, "r %d %lf %lf %lf %lf %s %s", &id, &x, &y, &w, &h, colorB,
-             colorF) == 7) {
+  if (sscanf(line, "r %d %lf %lf %lf %lf %s %s", &id, &x, &y, &w, &h, colorB, colorF) == 7) {
     Figure newFig = figureInit(RECTANGLE);
     setRectangle(newFig, id, x, y, w, h, colorB, colorF);
     return newFig;
@@ -51,8 +51,7 @@ static Figure processLine(const char *line) {
   int id;
   double x1, y1, x2, y2;
   char color[32];
-  if (sscanf(line, "l %d %lf %lf %lf %lf %s", &id, &x1, &y1, &x2, &y2, color) ==
-      6) {
+  if (sscanf(line, "l %d %lf %lf %lf %lf %s", &id, &x1, &y1, &x2, &y2, color) == 6) {
     Figure newFig = figureInit(LINE);
     setLine(newFig, id, x1, y1, x2, y2, color);
     return newFig;
@@ -63,14 +62,12 @@ static Figure processLine(const char *line) {
 static Figure processText(const char *line, Ts *t) {
   int id;
   double x, y;
-  char colorB[32], colorF[32];
-  char anchor;
+  char colorB[32], colorF[32], anchor;
   char text[MAX_LINE_BUFFER];
-  if (sscanf(line, "t %d %lf %lf %s %s %c %[^\n]", &id, &x, &y, colorB, colorF,
-             &anchor, text) == 7) {
+  
+  if (sscanf(line, "t %d %lf %lf %s %s %c %[^\n]", &id, &x, &y, colorB, colorF, &anchor, text) == 7) {
     Figure newFig = figureInit(TEXT);
-    setText(newFig, id, x, y, colorB, colorF, anchor, text, t->family,
-            t->weight, t->size);
+    setText(newFig, id, x, y, colorB, colorF, anchor, text, t->family, t->weight, t->size);
     return newFig;
   }
   return NULL;
@@ -79,14 +76,14 @@ static Figure processText(const char *line, Ts *t) {
 static void processTs(const char *line, Ts *t) {
   char family[64], weight[3];
   int size;
-  if (sscanf(line, "ts %s %s %d", family, weight, &size) != 3)
-    return;
-  strcpy(t->family, family);
-  strcpy(t->weight, weight);
-  t->size = size;
+  if (sscanf(line, "ts %s %s %d", family, weight, &size) == 3) {
+      strcpy(t->family, family);
+      strcpy(t->weight, weight);
+      t->size = size;
+  }
 }
 
-static void parseGeoLine(char *lineBuffer, Queue figureList, Ts *t) {
+static void parseGeoLine(char *lineBuffer, List figureList, Ts *t) {
   char command[16];
   Figure newFig = NULL;
 
@@ -104,19 +101,26 @@ static void parseGeoLine(char *lineBuffer, Queue figureList, Ts *t) {
   else if (strcmp(command, "t") == 0)
     newFig = processText(lineBuffer, t);
   else if (strcmp(command, "ts") == 0)
-    processTs(command, t);
-  if (newFig)
-    queueEnqueue(figureList, newFig);
+    processTs(lineBuffer, t);
+
+  if (newFig != NULL) {
+    listAddLast(figureList, newFig);
+  }
 }
 
-void processGeoFile(const char *geoFilePath, Queue figureList) {
+void processGeoFile(const char *geoFilePath, List figureList) {
   FILE *file = fopen(geoFilePath, "r");
-  if (!file)
+  if (!file) {
     return;
-  char line[MAX_LINE_BUFFER];
-  Ts *t = tsInit();
-  while (fgets(line, sizeof(line), file)) {
-    parseGeoLine(line, figureList, t);
   }
+
+  char lineBuffer[MAX_LINE_BUFFER];
+  Ts *t = tsInit();
+
+  while (fgets(lineBuffer, sizeof(lineBuffer), file)) {
+    parseGeoLine(lineBuffer, figureList, t);
+  }
+
+  free(t);
   fclose(file);
 }
--- src/geo.h
+++ src/geo.h
@@ -1,14 +1,14 @@
 #ifndef GEO_H
 #define GEO_H
 
-#include "queue.h"
+#include "list.h"
 
 /**
  * @brief Processa um arquivo .geo, lendo as figuras e as adicionando
  * a uma lista.
  * @param geoFilePath O caminho completo para o arquivo .geo a ser lido
- * @param figureList A Queue onde as figures criadas serão armazenadas.
+ * @param figureList A Lista (List) onde as figures criadas serão armazenadas.
  */
-void processGeoFile(const char *geoFilePath, Queue figureList);
+void processGeoFile(const char *geoFilePath, List figureList);
 
 #endif // GEO_H
